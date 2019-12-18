f66086db635243ccdfc95dd4099ab3dd78f4d0bc
Stefan Paraschiv <stefan.paraschiv@tremend.com>
2019-12-18 13:17:54 +0200

    users model

---
+++ src/main/java/com/tremend/project/model/Users.java
@@ -0,0 +1,67 @@
+package com.tremend.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Date;
+
+@Entity
+@Table(name = "USERS")
+public class Users {
+    @Id
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private int prefTemp;
+    private Date onTime;
+    private Date offTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPrefTemp() {
+        return prefTemp;
+    }
+
+    public void setPrefTemp(int prefTemp) {
+        this.prefTemp = prefTemp;
+    }
+
+    public Date getOnTime() {
+        return onTime;
+    }
+
+    public void setOnTime(Date onTime) {
+        this.onTime = onTime;
+    }
+
+    public Date getOffTime() {
+        return offTime;
+    }
+
+    public void setOffTime(Date offTime) {
+        this.offTime = offTime;
+    }
+}
